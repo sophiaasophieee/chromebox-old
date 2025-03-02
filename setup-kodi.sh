0a5c5ff717ca9b22d4f66e3a60a2edfb58966570
Sophie <sophiaasophieee@gmail.com>
2025-03-02 11:46:33 -0500

    Add files via upload

---
+++ setup-kodi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# This script will prep supported
+# ChromeOS devices for Kodi installation via
+# LibreELEC or GalliumOS/Ubuntu
+#
+# Created by Mr.Chromebox <mrchromebox@gmail.com>
+#
+# May be freely distributed and modified as needed,
+# as long as proper attribution is given.
+#
+
+echo "This script is no longer supported; instead please use the Firmware"
+echo "Utility Script as per the instructions on MrChromebox.tech"
